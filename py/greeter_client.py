f28194e771cb3fc779362489ebd95aa00e4669d7
Joshua Zhang <toruta39@gmail.com>
2021-12-27 16:08:45 +0900

    implement simple server and client

---
+++ py/greeter_client.py
@@ -0,0 +1,18 @@
+import logging
+
+import grpc
+import greeter_pb2
+import greeter_pb2_grpc
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = greeter_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(greeter_pb2.HelloRequest(name='you'))
+        print("Greeter client received: " + response.message)
+        response = stub.SayHelloAgain(greeter_pb2.HelloRequest(name='you'))
+        print("Greeter client received: " + response.message)
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
+    
---
+++ py/greeter_server.py
@@ -0,0 +1,24 @@
+from concurrent import futures
+import logging
+
+import grpc
+import greeter_pb2
+import greeter_pb2_grpc
+
+class Greeter(greeter_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        return greeter_pb2.HelloReply(message="Hello, %s!" % request.name)
+
+    def SayHelloAgain(self, request, context):
+        return greeter_pb2.HelloReply(message="Hello again, %s!" % request.name)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    greeter_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
